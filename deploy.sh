065e27aa596af76658602dbe246aeba2ba52284e
IldarDavletyarov <ildardaf@gmail.com>
2022-01-30 03:40:50 +0500

    Add deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+npm run build
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:IldarDavletyarov/storyboarder.git master:gh-pages
+
+cd -

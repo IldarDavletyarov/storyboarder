38d5b6d57399106f533cd3a7fa3f8989efd6ad8e
IldarDavletyarov <ildardaf@gmail.com>
2022-01-30 20:54:54 +0500

    deploy

---
+++ precache-manifest.e2b21d388585142c73cd641de1f0803b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a2b58df330c85e0029eecf71d7c26",
+    "url": "/storyboarder/_redirects"
+  },
+  {
+    "revision": "2f832fdd248c0c2de6b51ab051148de9",
+    "url": "/storyboarder/android-chrome-192x192.png"
+  },
+  {
+    "revision": "a5a453f7db22427bd51ff46f1aa48665",
+    "url": "/storyboarder/android-chrome-512x512.png"
+  },
+  {
+    "revision": "33ca68b2f93b1e6965fbf71912872065",
+    "url": "/storyboarder/apple-touch-icon.png"
+  },
+  {
+    "revision": "8f7d52b924d82cc4f417",
+    "url": "/storyboarder/css/app.a58221db.css"
+  },
+  {
+    "revision": "0c1fbfd1ac8844e3100c",
+    "url": "/storyboarder/css/chunk-vendors.0c1b19d1.css"
+  },
+  {
+    "revision": "016750af6018278f5987d1a726be9443",
+    "url": "/storyboarder/favicon-16x16.png"
+  },
+  {
+    "revision": "cd3dfa1ae31102cf750bc79d84cb15f4",
+    "url": "/storyboarder/favicon-32x32.png"
+  },
+  {
+    "revision": "d7c5743a11c038f62d4d9300cb3de9d3",
+    "url": "/storyboarder/frame.jpg"
+  },
+  {
+    "revision": "ddb7b0093def5f356e8b77108aed4417",
+    "url": "/storyboarder/index.html"
+  },
+  {
+    "revision": "8f7d52b924d82cc4f417",
+    "url": "/storyboarder/js/app.9ec1612a.js"
+  },
+  {
+    "revision": "0c1fbfd1ac8844e3100c",
+    "url": "/storyboarder/js/chunk-vendors.e3634520.js"
+  },
+  {
+    "revision": "c72eb4dbfc867d2594a891dac4275495",
+    "url": "/storyboarder/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/storyboarder/robots.txt"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/storyboarder/site.webmanifest"
+  },
+  {
+    "revision": "a198f4fe7ab97cba5900b7e61b48a863",
+    "url": "/storyboarder/stat.svg"
+  }
+]);
